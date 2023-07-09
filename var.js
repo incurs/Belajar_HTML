e6ca45e0dbee578eeca46226972a0af6d151d166
Rizal <70750087+incurs@users.noreply.github.com>
2023-07-09 13:26:05 +0700

    Add files via upload

---
+++ var.js
@@ -0,0 +1,10 @@
+let name = 'John'; // Deklarasi variabel name dengan nilai 'John'
+const age = 25; // Deklarasi variabel age dengan nilai 25
+var city = 'New York'; // Deklarasi variabel city dengan nilai 'New York'
+
+console.log(name); // Output: John
+console.log(age); // Output: 25
+console.log(city); // Output: New York
+
+name = 'Jane'; // Mengubah nilai variabel name
+console.log(name); // Output: Jane
